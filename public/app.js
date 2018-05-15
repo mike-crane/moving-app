e6f0047953c10edc36b0732da5e9582979104594
mike crane <mike.c0321@gmail.com>
2018-05-14 21:51:56 -0400

    build landing, packing and unpacking screens

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ const MOCK_BOXES = {
       "description": "Stuff from bookshelf", 
       "contents": "books, picture frames", 
       "packed": 1470016976609, 
-      "unpacked": "false" 
+      "unpacked": false 
     },
     {
       "id": "2222222",
@@ -16,7 +16,7 @@ const MOCK_BOXES = {
       "description": "Bakeware and plates",
       "contents": "pizza pan, cookie sheet, muffin pan, large dishes, bowls",
       "packed": 1470012976609,
-      "unpacked": "false"
+      "unpacked": true
     },
     {
       "id": "3333333",
@@ -25,7 +25,7 @@ const MOCK_BOXES = {
       "description": "Toys and posters",
       "contents": "legos, drone, fidget spinner, scooter, star wars poster, Labron James poster",
       "packed": 1470011976609,
-      "unpacked": "false"
+      "unpacked": false
     },
     {
       "id": "4444444",
@@ -34,10 +34,23 @@ const MOCK_BOXES = {
       "description": "Tools and recreation equipment",
       "contents": "circular saw, hammer, wrench, bocce ball set, soccer ball, kite",
       "packed": 1470009976609,
-      "unpacked": "false"
-    }
+      "unpacked": false
+    },
+    {
+      "id": "5555555",
+      "user": "Jeff",
+      "room": "Office",
+      "description": "Office equipment and supplies",
+      "contents": "computer, monitor, files, stapler, paper, pens",
+      "packed": 1470013976609,
+      "unpacked": true
+    },
   ]
 };
+
+$('.message').on('click', 'a', function () {
+  $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+})
 
 function getBoxes(callbackFn) {
   setTimeout(function() { callbackFn(MOCK_BOXES)}, 100);
@@ -45,12 +58,15 @@ function getBoxes(callbackFn) {
 
 function displayBoxes(data) {
   for (index in data.boxes) {
-    $('body').append(
-      `<h2>${data.boxes[index].room}</h2>
-      <p>${data.boxes[index].description}</p>
-      <p>${data.boxes[index].contents}</p>
-      <hr>`
-    );
+    if (data.boxes[index].unpacked) {
+      $('.unpacked-list').append(
+        `<li class="unpacked-item">${data.boxes[index].description} from ${data.boxes[index].room} <div class="box-status"><label for="checkBox">Unpacked:</label><input id="checkBox" type="checkbox"></div><ul><li class="box-contents">${data.boxes[index].contents}</li></ul></li>`
+      );
+    } else {
+      $('.packed-list').append(
+        `<li class="packed-item">${data.boxes[index].description} from ${data.boxes[index].room} <div class="box-status"><a href="#">edit / delete</a></div><ul><li class="box-contents">${data.boxes[index].contents}</li></ul></li>`
+      );
+    }
   }
 }
 
