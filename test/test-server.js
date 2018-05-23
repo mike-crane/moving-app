ec441d97f4cc49be458db67b3fe3240ffffe329b
mike crane <mike.c0321@gmail.com>
2018-05-23 11:33:51 -0400

    fixed chai-http versioning error

--- test/test-server.js
+++ test/test-server.js
@@ -2,13 +2,23 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server.js');
+const { app, runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
 
 const should = chai.should();
 
 chai.use(chaiHttp);
 
 describe('index.html', function () {
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  after(function () {
+    return closeServer();
+  });
+
   it('should exist', function () {
     return chai.request(app)
       .get('/')
